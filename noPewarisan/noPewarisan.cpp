b48565f3190f0f848a5d90b20df2d699ac57be62
jaddu17 <shafajaddu@gmail.com>
2024-05-28 10:46:25 +0700

    membuat prosedur memanggil

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -9,3 +9,20 @@ public:
 		cout << "Hallo saya Function dari base Class";
 	}
 };
+
+class derivedClass : public baseClass
+{
+public:
+	void perkenalan()
+	{
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
+
+int main()
+{
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
