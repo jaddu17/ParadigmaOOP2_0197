39d8f73f0047c3a33e4c600b23e80f946f75f422
jaddu17 <shafajaddu@gmail.com>
2024-05-28 10:14:18 +0700

    membuat virtual function biasa

--- ParadigmaOOP2.cpp
+++ ParadigmaOOP2.cpp
@@ -4,5 +4,12 @@ using namespace std;
 class seseorang
 {
 public:
+	// pure virtual function
 	virtual void pesan() = 0;
+
+	// virtual function biasa
+	// virtual void pesan()
+	// {
+	//		cout << "Pesan dari seseoang" << endl;
+	// }
 };
